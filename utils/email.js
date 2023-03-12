d0e49b86889c4da4fd3b604da579be59661e8163
dhanno75 <pdhananjay774@gmail.com>
2023-03-12 18:34:06 +0530

    some minor changes

--- utils/email.js
+++ utils/email.js
@@ -1,26 +1,4 @@
 import nodemailer from "nodemailer";
-// import sgMail from "@sendgrid/mail";
-
-// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-// const msg = {
-//   to: "p-dhananjay@outlook.com",
-//   from: "pdhananjay774@gmail.com",
-//   subject: "Sending with SendGrid is Fun",
-//   text: "and easy to do anywerher, even with Node.js",
-//   html: "<strong>and easy to do anywhere, even with Node.js</strong>",
-// };
-
-// sgMail
-//   .send(msg)
-//   .then(() => {
-//     console.log("Email sent");
-//   })
-//   .catch((error) => {
-//     console.error(error);
-//   });
-
-// const sendEmail = async (options) => {};
 
 export const sendEmail = async (options) => {
   // Create a transporter
